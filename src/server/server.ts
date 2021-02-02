bceddf40c381e876281e910714dda39090851249
Taehoon Yoon <yooontehoon@naver.com>
2021-02-02 19:53:41 +0900

    기본 세팅.

---
+++ src/server/server.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import http from 'http';
+import path from 'path';
+
+const port: number = 3000;
+
+class App {
+    server: http.Server;
+    port: number;
+
+    constructor(port: number) {
+        this.port = port;
+        const app = express();
+        app.use(express.static(path.join(__dirname, '../client')));
+        app.use('/build/three.module.js', express.static(path.join(__dirname, '../../node_modules/three/build/three.module.js')));
+        app.use('/jsm/controls/OrbitControls', express.static(path.join(__dirname, '../../node_modules/three/examples/jsm/controls/OrbitControls.js')));
+
+        this.server = new http.Server(app);
+    }
+    public Start() {
+        this.server.listen(this.port, ()=> {
+            console.log(`Server listening on port ${this.port}.`)
+        })
+    }
+}
+
+new App(port).Start();
